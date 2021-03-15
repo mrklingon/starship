e64d2bf6e03f83d0e6a10d70ffaf5184281d4f34
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-15 17:10:10 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,16 @@
-let star = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . 1 . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 9 . 1 . 9 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 1 . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+effects.starField.startScreenEffect()
 scene.setBackgroundColor(15)
-star.setPosition(72, 8)
 let vger = sprites.create(assets.image`Voyager`, SpriteKind.Player)
+let kzon = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
+kzon.setPosition(34, 4)
+kzon.setBounceOnWall(true)
 controller.moveSprite(vger)
-star.startEffect(effects.blizzard, 500)
+vger.setBounceOnWall(true)
+let missle = sprites.createProjectileFromSprite(assets.image`missle`, kzon, 50, 50)
 forever(function () {
-	
+    kzon.x = vger.x
+    pause(200)
+    if (randint(0, 30) < 5) {
+        missle = sprites.createProjectileFromSprite(assets.image`missle`, kzon, vger.x, vger.y)
+    }
 })
