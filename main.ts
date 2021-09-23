0a457d65d92c29d632d02d00ed9457fd3a5ec0b2
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-09-22 20:24:11 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    missle = sprites.createProjectileFromSprite(assets.image`missle0`, vger, 0, -100)
+    Vmiss = sprites.createProjectileFromSprite(assets.image`missle0`, vger, 0, -100)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    if (sprite == missle) {
+        info.changeLifeBy(-1)
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (otherSprite == kzon) {
+        info.changeScoreBy(1)
+    }
 })
 let missle: Sprite = null
+let Vmiss: Sprite = null
+let kzon: Sprite = null
 let vger: Sprite = null
 effects.starField.startScreenEffect()
 game.splash("USS Voyager", "Lost in the Delta Quadrant - needs to avoid the Kazon!")
 scene.setBackgroundColor(15)
 vger = sprites.create(assets.image`Voyager`, SpriteKind.Player)
-let kzon = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
+kzon = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
 kzon.setPosition(34, 4)
 kzon.setBounceOnWall(true)
 controller.moveSprite(vger)
 vger.setBounceOnWall(true)
+info.setLife(5)
 forever(function () {
     kzon.x = vger.x
     pause(200)
